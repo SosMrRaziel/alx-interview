6380295615b4991c11278b6f8a7463c6b3520822
SosMrRaziel <lbt_alh@hotmail.fr>
2024-06-21 03:19:53 +0100

    stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,50 +1,47 @@
 #!/usr/bin/python3
-"""
-log parsing
-"""
+""" a script that reads stdin line by line and computes metrics """
 
 import sys
 import re
 
+
 def output(log: dict) -> None:
-    """
-    Helper function to display stats.
-    """
+    """ Output the log data """
     print("File size: {}".format(log["file_size"]))
     for code in sorted(log["code_frequency"]):
         if log["code_frequency"][code]:
             print("{}: {}".format(code, log["code_frequency"][code]))
 
+
 if __name__ == "__main__":
     # Regular expression to match log lines
     regex = re.compile(
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
 
     line_count = 0
     log = {}
     log["file_size"] = 0
     log["code_frequency"] = {
-        str(code): 0 for code in [200, 301, 400, 401, 403, 404, 405, 500]}
+        str(code): 0 for code in [
+            200, 301, 400, 401, 403, 404, 405, 500]}
 
     try:
         for line in sys.stdin:
             line = line.strip()
             match = regex.fullmatch(line)
-            if match:
+            if (match):
                 line_count += 1
                 code = match.group(1)
                 file_size = int(match.group(2))
 
-                # Accumulate file size
+                # File size
                 log["file_size"] += file_size
 
-                # Update status code frequency
-                if code.isdecimal():
+                # status code
+                if (code.isdecimal()):
                     log["code_frequency"][code] += 1
 
-                # Display stats every 10 lines
-                if line_count % 10 == 0:
+                if (line_count % 10 == 0):
                     output(log)
     finally:
-        # Display final stats
         output(log)
