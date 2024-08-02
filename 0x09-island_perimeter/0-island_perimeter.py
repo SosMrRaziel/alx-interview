e4bb0b36f87af8831e7e2a45ace828e9d4a3262e
sosmrraziel <lbt_alh@hotmail.fr>
2024-08-02 15:15:01 +0100

    Island Perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Island perimeter"""
+
+
+def island_perimeter(grid):
+    peri = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                peri += 4 - (
+                    (i > 0 and grid[i - 1][j])
+                    + (i < len(grid) - 1 and grid[i + 1][j])
+                    + (j > 0 and grid[i][j - 1])
+                    + (j < len(grid[0]) - 1 and grid[i][j + 1])
+                )
+    return peri
