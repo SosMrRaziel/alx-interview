f77d5761e31a4a53b3fb02f9e9e34a9d4a572936
sosmrraziel <lbt_alh@hotmail.fr>
2024-08-08 14:57:36 +0100

    task0

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,26 +1,35 @@
 #!/usr/bin/python3
 """Prime Game"""
 
+def primeNumbers(n):
+    """Returns a list of prime numbers up to n"""
+    primeNos = []
+    filtered = [True] * (n + 1)
+    for prime in range(2, n + 1):
+        if (filtered[prime]):
+            primeNos.append(prime)
+            for i in range(prime, n + 1, prime):
+                filtered[i] = False
+    return primeNos
+
+
 def isWinner(x, nums):
-    """ Initialize counters for Maria and Ben's wins """
-    maria_wins = 0
-    ben_wins = 0
-
-    # Play each round of the game
-    for n in nums:
-        # Calculate the sum of prime numbers in the set
-        prime_sum = sum([i for i in range(2, n + 1) if all(i % j != 0 for j in range(2, int(i**0.5) + 1))])
-
-        # Determine the winner of the game
-        if prime_sum % 2 == 0:
-            ben_wins += 1
-        else:
-            maria_wins += 1
-
-    # Compare the wins and return the winner
-    if ben_wins > maria_wins:
-        return "Ben"
-    elif maria_wins > ben_wins:
-        return "Maria"
-    else:
+    """Determine who the winner of the game is
+       Args:
+        x (int): number of rounds
+        nums (list): list of integers
+    """
+    if x is None or nums is None or x == 0 or nums == []:
         return None
+    Maria = Ben = 0
+    for i in range(x):
+        primeNos = primeNumbers(nums[i])
+        if len(primeNos) % 2 == 0:
+            Ben += 1
+        else:
+            Maria += 1
+    if Maria > Ben:
+        return 'Maria'
+    elif Ben > Maria:
+        return 'Ben'
+    return None
